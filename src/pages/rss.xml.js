7ef532487fcd5aa37cacd662d6e8166c76f4d8b5
Intrepid Toucan <96765951+IntrepidToucan@users.noreply.github.com>
2024-11-16 16:15:42 -0800

    fix: fix post glob for RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { SITE_TITLE } from '../utils';
 
 export async function GET(context) {
   return rss({
-    title: 'Making Mythology',
-    description: 'Worldbuilding, animation, and music composition',
+    title: SITE_TITLE,
+    description: 'Fantasy worldbuilding, music composition, and 3D modeling',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./works/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./works/*.md?(x)')),
     customData: `<language>en-us</language>`,
   });
 }
